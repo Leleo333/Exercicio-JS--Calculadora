c82c26e78723abee14b8661d13e385e4da174085
unknown <leo.vemcedor@gamil.com>
2022-11-28 08:42:27 -0300

    Exercicio JS-Calculadora

---
+++ calculadora.js
@@ -0,0 +1,22 @@
+function soma (a, b){
+    return a + b;
+}
+
+function subtração (a, b) {
+
+    return a - b;
+}
+
+
+function divisão (a, b) {
+
+    return a / b;
+}
+
+
+function multi (a, b) {
+
+    return a * b;
+}
+
+console.log (soma(10,10));
